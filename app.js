304262cae63adad4707773e47af01bbaaa4a97fb
Spencer Marx <smarx@humancaresystems.com>
2020-11-05 14:34:07 -0500

    Add sheet page and range

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const fs = require('fs');
 const utils = require('./lib/helpers/index.helper');
 const tasks = require('./lib/spreadsheet-tasks/index.task');
 
-const startingPoint = 'Sheet1!A3';
+const sheet = 'SMS';
+const range = 'A:Z'
 
 const main = async () => {
   try {
@@ -14,7 +15,7 @@ const main = async () => {
       if (err) return console.log('Error loading client secret file:', err);
       // Authorize a client with credentials, then call the Google Sheets API.
       utils.authorize(JSON.parse(content), auth => {
-        tasks.readRows(auth);
+        tasks.readRows(auth, `${sheet}!${range}`);
       });
     });
   } catch (error) {
